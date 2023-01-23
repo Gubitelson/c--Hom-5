fe46c018638ad501254c4ae2c26c664e5588edbe
Gubitelson <119538200+Gubitelson@users.noreply.github.com>
2023-01-23 20:55:24 +0400

    Initial commit

---
+++ #1/Program.cs
@@ -0,0 +1,43 @@
+﻿/*  Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+ Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+ */
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void ArraysEvenNumbsAmount(int[] array)
+
+{
+    int count = 0;
+    Console.Write("even numbers in array: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            Console.Write($"{array[i]}; ");
+            count += 1;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"amount of even numbers in your array:{count}");
+}
+
+int[] array = CreateRandomArray(6, 100, 1000);
+ArraysEvenNumbsAmount(array);
+Console.Write("Your array: ");
+ShowArray(array); 
---
+++ #2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*  Задача 36: Задайте одномерный массив, заполненный случайными числами.
+  Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+ */
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void ArraysUnevenPosNumbs(int[] array)
+{
+    int sum = 0;
+    Console.Write("Numbers on uneven positions in array: ");
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        Console.Write($"{array[i]}; ");
+        sum += array[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Sum of number on uneven positions of your array: {sum}");
+}
+int[] array = CreateRandomArray(8, 100, 1000);
+ArraysUnevenPosNumbs(array);
+Console.Write("Your array: ");
+ShowArray(array); 
